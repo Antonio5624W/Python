93463f8992b5d60830d3ec806f94e24a842bc39d
Antonio5624W <chui.jaso@gmail.com>
2025-05-14 19:13:32 -0700

    Operators

--- 02_Operators.py
+++ 02_Operators.py
@@ -1,9 +1,24 @@
-### Operadores ###
+### Operadores  arimeticos ##
 #Utilizamos los prints para mostrar los resultados de las operaciones
 print(3+4)
 print(3-4)
 print(3%4)
 print(3*4)
 print(3/4)
-print(3>4)
-print(3<4)
+print(3 // 4)
+print(3 ** 4 + 3 - 7 / 1 // 4 )
+
+print("Hola " + "Python " + "¿Qué tal?")
+print("Hola " + str(5))
+print("Hola " * 5)
+print("Hola " * (2 ** 3))
+
+my_float = 2.5 * 2 
+print("Hello " * int (my_float))
+### Operadores  Comparativos ##
+print (3 > 5)
+print (5 < 4)
+print (5 >= 5)
+print (5 <= 5)
+print (5 == 9)
+
